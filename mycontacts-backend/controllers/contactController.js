b5a985500e7d5455ea152f4af7c8de358398c1bc
alyssa <allyer88@gmail.com>
2024-09-09 21:55:44 -0700

    CRUD update

--- mycontacts-backend/controllers/contactController.js
+++ mycontacts-backend/controllers/contactController.js
@@ -1,14 +1,16 @@
 //make it async to work with Mongodb
 //This can handle error while using async
 const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel")
 
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
 const getContacts = asyncHandler(async(req, res)=>{
-    res.status(200).json({
-        message: "Get all contacts"
-    });
+    //When you call find() without passing any filter or query object,
+    //Mongoose retrieves all documents in the collection. In this case, it gets all contacts from the "contacts" collection.
+    const contacts = await Contact.find();
+    res.status(200).json(contacts);
 });
 
 //@desc Create NEW contacts
@@ -17,42 +19,60 @@ const getContacts = asyncHandler(async(req, res)=>{
 const createContact = asyncHandler(async(req, res)=>{
     console.log("The request body is ", req.body);
     //destructuring assignments to extract values from objects and arrays and assign them to variables.
-    const {name, age, email} = req.body;
-    if(!name || !age || !email){
+    const {username, email} = req.body;
+    if(!username || !email){
         //set status to error
         res.status(400);
         throw new Error("Please provide all fields");
     }
-    res.status(201).json({
-        message: "Create contacts"
+    //if there is data
+    const contact = await Contact.create({
+        username,
+        email
     });
+    res.status(201).json(contact);
 });
 
 //@desc Get specific contact
 //@route GET /api/contacts/:id
 //@access public
 const getContact = asyncHandler(async(req, res)=>{
-    res.status(200).json({
-        message: `Get contact for ${req.params.id}`
-    });
+    const contact = await Contact.findById(req.params.id);
+    if(!contact){
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+    res.status(200).json(contact);
 });
 
 //@desc update specific contact
 //@route PUT /api/contacts/:id
 //@access public
 const updateContact = asyncHandler(async(req, res)=>{
-    res.status(200).json({
-        message: `Update contact for ${req.params.id}`
-    });
+    const contact = await Contact.findById(req.params.id);
+    if(!contact){
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new : true}
+    );
+    res.status(200).json(updatedContact);
 });
 
 //@desc delete specific contact
 //@route DELETE /api/contacts/:id
 //@access public
 const deleteContact = asyncHandler(async(req, res)=>{
-    res.status(200).json({
-        message: `Delete contact for ${req.params.id}`
-    });
+    const contact = await Contact.findById(req.params.id);
+    if(!contact){
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+    await Contact.deleteOne();
+    res.status(200).json(contact);
 });
 
 //Importing and exporting multiple named modules using {}.
---
+++ mycontacts-backend/models/contactModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+//schema is a blueprint that defines how a database is organized and structured
+const contactSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please add the contact name"],
+    },
+    email: {
+        type: String,
+        required: [true, "Please add the email"],
+    },
+}, {
+    timestamps: true,
+});
+
+//This method creates a Mongoose model.
+//"Contact" is the name of the model. It represents the collection name in MongoDB.
+//contactSchema is the schema you previously defined, which sets the structure for the documents in the collection 
+module.exports = mongoose.model("Contact", contactSchema);
